418beaff7b807d765c770ce2c7f2c7ff47873856
krela <krel.andrej@yandex.ru>
2023-03-14 10:34:35 +0700

    1 комит

---
+++ src/main/java/pro/calcu/calculatorDZ/CalculatorDzApplication.java
@@ -0,0 +1,13 @@
+package pro.calcu.calculatorDZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorDzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorDzApplication.class, args);
+	}
+
+}
